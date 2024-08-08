f873a10aaccd1294f9be0fa2c70a0e8d8d6ec964
Cheril Shah <79891166+Cheril311@users.noreply.github.com>
2024-08-08 18:16:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'pretrain=scripts.pretrain:main',
+            'pretrain=scripts.pretrainLSL:main',
             'posttrain=scripts.posttrain:main',
         ],
     },
